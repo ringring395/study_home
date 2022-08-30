7ecc66e2e86a8ad057588bfcc48d4cb7f3f04f97
ringring <ringring395@gmail.com>
2022-08-30 13:49:45 +0900

    08/30 13;49

--- springex/src/main/java/com/ring/controller/BoardController.java
+++ springex/src/main/java/com/ring/controller/BoardController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ring.model.AttachFileVO;
